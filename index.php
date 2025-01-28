e7e8f262754ec3446e741817827385358122ac3e
MENG Vuthvitou <liltou081@gmail.com>
2025-01-28 14:02:26 +0700

    Fetch Student from db

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php 
-    include("./db/config.php");
+    include "./db/config.php";
+
+    $sql = "SELECT * FROM students";
+    $results = $cnn -> query($sql);    
 ?>
 
 <!DOCTYPE html>
@@ -10,6 +13,42 @@
     <title>Document</title>
 </head>
 <body>
-    Hello
+    <header>
+        <h1>Student List</h1>
+
+        <ul>
+            <li><a href="">View</a></li>
+            <li><a href="">Add</a></li>
+            <li><a href="">Edit</a></li>
+        </ul>
+    </header>
+
+    <main>
+        <table border="1">
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>Course</th>
+                <th>Actions</th>
+            </tr>
+        <?php while ($row = $results->fetch_assoc()): ?>
+            <tr>
+                <td><?php echo $row['id']; ?></td>
+                <td><?php echo $row['name']; ?></td>
+                <td><?php echo $row['email']; ?></td>
+                <td><?php echo $row['phone']; ?></td>
+                <td><?php echo $row['course']; ?></td>
+                <td>
+                    <a href="edit_student.php?id=<?php echo $row['id']; ?>">Edit</a>
+                    <a href="delete_student.php?id=<?php echo $row['id']; ?>">Delete</a>
+                </td>
+            </tr>
+        <?php endwhile; ?>
+    </table>
+    </main>
+
+
 </body>
 </html>
